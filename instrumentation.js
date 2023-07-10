6c9a3860ee33f74d4fb096653ea03f961366f30d
jiyoungglee <jiyoung.g.lee118@gmail.com>
2023-07-10 11:10:04 -0400

    otel server skeleton

--- instrumentation.js
+++ instrumentation.js
@@ -1,11 +1,13 @@
 export async function register() {
+  console.log('registered');
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./instrumentation.node')
+    await import('./instrumentation.node');
+    console.log('tracing')
   }
 }
 
 // import { registerOTel } from '@vercel/otel'
- 
+
 // export function register() {
 //   registerOTel('next-app')
 // }
---
+++ instrumentation.node.js
@@ -0,0 +1,51 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { ConsoleSpanExporter, BatchSpanProcessor} = require('@opentelemetry/sdk-trace-node');
+const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+import { Resource } from '@opentelemetry/resources'
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions'
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+
+
+const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'NextJS-Demo',
+  }),
+  spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter({url: 'http://localhost:8080'})),
+  instrumentations: [new HttpInstrumentation()]
+})
+
+sdk.start()
+
+
+// const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+// const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
+// const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+// const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+
+// const sdk = new NodeSDK({
+//   traceExporter: new OTLPTraceExporter(),
+//   resource: new Resource({
+//     [SemanticResourceAttributes.SERVICE_NAME]: 'next-app',
+//   }),
+//   metricReader: new PeriodicExportingMetricReader({
+//     exporter: new BatchSpanProcessor(new ConsoleMetricExporter())
+//   }),
+//   spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter({url: 'http://localhost:8080'})),
+//   instrumentations: [new HttpInstrumentation()]
+// });
+
+// sdk.start()
+
+
+
+// const provider = new NodeTracerProvider();
+
+// const exporter = new OTLPTraceExporter();
+// provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+// provider.register();
+
+
+// registerInstrumentations({
+//   instrumentations: [new HttpInstrumentation()],
+// });
