1ad30af7226650f78eab8d96511947a56b7658a9
CO <99570414+codeFromCO@users.noreply.github.com>
2023-07-11 23:17:20 -0400

    main page, dynamic route

---
+++ app/components/Pokemon.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import Link from 'next/Link'
+
+async function getPokemon() {
+  // fetch request to pokemon api
+  let response = await fetch('https://pokeapi.co/api/v2/pokemon');
+
+  let usablePokemonInfo = response.json();
+  // this will return an object with count, next, previous and results; results an array of 20 pokemon objects each with a name and url
+
+  return usablePokemonInfo;
+}
+
+const PokemonComponent = () => {
+  const [pokemon, setPokemon] = useState(null);
+
+  const fetchAllPokemon = async () => {
+    const starterPokemon = await getPokemon();
+    setPokemon(starterPokemon.results);
+  };
+
+  useEffect(() => {
+    fetchAllPokemon();
+  }, []);
+
+  if (pokemon) {
+    const pokemonData = pokemon.map((pokemon, i) => {
+      console.log(pokemon.name, i)
+
+      return (
+        <div className='nameCard'>
+          <p className='pokemonName'>{pokemon.name}</p>
+          {/* <button id={pokemon.name} onClick={test}>get stats</button> */}
+          <div key={pokemon.name}>
+          <Link href={`/whosthatpokemon/${pokemon.name}`} style={{color: 'red'}}>get stats</Link>
+          </div>
+        </div>
+      );
+  });
+
+  function test() {
+    return console.log('in the get stats button')
+  }
+
+    return (
+      <div className="pokemonNameContainer">
+       {pokemonData}
+      </div>
+    );
+  }
+};
+
+export default PokemonComponent;
+
+// cards on home page, clicking it should take you to /ditto and display info
+// find more pokemon and if you like it it gets saved to your home page?
