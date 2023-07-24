7def81ac6134f79858649d43e663d4e73ea38a34
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-07-23 22:13:28 -0400

    Separated test app

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,12 +1,18 @@
 'use client'
 
+// import dynamic from 'next/dynamic';
 // below not working 
 // import * as app from 'nvision'
+
+import * as Dashboard from 'nvision';
+
+// const Dashboard = dynamic(() => import('nvision'), { ssr: false });
 
 export default function TestPage() {
     return (
         <div>
             <h1>Hello</h1>
+            <Dashboard/>
         </div>
     )
 }
