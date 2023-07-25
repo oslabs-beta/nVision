6c1542004567266304ffd112407c39ecf0d4be23
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-07-25 00:45:55 -0400

    npmdashboard to typescript

--- app/npmdashboard/page.tsx
+++ app/npmdashboard/page.tsx
@@ -1,10 +1,7 @@
 'use client';
-import React from 'react';
+
 import dynamic from 'next/dynamic';
 import {Dashboard} from 'nvision'
-// below not working 
-// import * as app from 'nvision'
-
 
 // const Dashboard = dynamic(() => import('nvision'), { ssr: false });
 
