9c0fe88f375c0226c8931ea8ceeb8d0f1a624ffb
jiyoungglee <jiyoung.g.lee118@gmail.com>
2023-07-25 20:40:05 -0400

    Added Dashboard Components. Add API for fileparser. Remove local npm package install commands

--- app/api/fileParser/route.ts
+++ app/api/fileParser/route.ts
@@ -4,13 +4,7 @@ import path from 'path'
 export async function GET(request: Request) {
     try {
         const appDirPath = path.join(process.cwd(), 'app')
-        
-        // (__dirname, "../../../../../../../", "app")
-        
         const results = await fileParser(appDirPath)
-
-        console.log('file parser results:',results)
-
         return new Response(JSON.stringify(results))
     }
     catch(e) {
--- app/dashboard/Wrapper.tsx
+++ app/dashboard/Wrapper.tsx
@@ -1,14 +1,14 @@
 'use client'
 
+// Creates client component wrapper for imported dashboard
+
 import Dashboard from "nvision"
 
-const Test = (props) => {
-    
+const Wrapper = (props) => {
     const {info} = props
-
     return (
         <Dashboard info={info} />
     )
 }
 
-export default Test
+export default Wrapper;
