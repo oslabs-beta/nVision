0efa170b78a0c944cc8168cd363a3b91d8c797c0
jiyoungglee <jiyoung.g.lee118@gmail.com>
2023-07-26 22:53:49 -0400

    add feature details and copy function to code mockup. additional styling.

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,21 +1,25 @@
 import type { Component } from 'solid-js';
+import setMode from '../setMode';
 
 interface CardProps {
-  image: string;
+  darkImage: string;
+  lightImage: string;
   feature: string;
   featureDes: string;
 }
 const FeatureCard: Component<CardProps> = ({
-  image,
+  darkImage,
+  lightImage,
   feature,
   featureDes,
 }: CardProps) => {
+  const { darkMode } = setMode;
   return (
     <>
       <div class='max-w-xs'>
         <div class='avatar mx-[15px] mt-[20px]'>
-          <div class='w-32 rounded-xl bg-base p-4 opacity-50 shadow-sm border-box border-2 border-solid border-white/[.75] dark:bg-[#252529] dark:opacity-100 dark: border-0'>
-            <img src={image} />
+          <div class='w-32 rounded-xl bg-base/[.5] p-4 shadow-sm border-box border-2 border-solid border-white/[.75] dark:bg-[#252529] dark:opacity-100 dark: border-0'>
+            <img src={darkMode() ? darkImage : lightImage} />
           </div>
         </div>
         <div class='py-[8px]'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,21 @@ module.exports = {
       colors: {
         'base': '#adadeb',
       },
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
     },
   },
   darkMode: 'class',
