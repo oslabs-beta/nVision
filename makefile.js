453b25bcc3be4382c4f9e90e0c148b052eea48e1
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-07-27 11:39:40 -0400

    Clean npmserver package

--- makefile.js
+++ makefile.js
@@ -6,8 +6,5 @@ const path = require('path');
 if (process.env.MY_APP_RUNNING) {
     console.log('not making files')
 } else {
-    const devContent = fs.readFileSync(path.join(__dirname, 'nvisionServer.js'), 'utf8')
-    fs.writeFileSync('nvisionServer.js', devContent)
-    console.log('added a server file')
-}
 
+}
