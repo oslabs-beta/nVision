4d2030876145435a98114b6ff1f600418651713d
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-08-09 18:24:34 -0400

    Clean demo app. No nvision files added

--- instrumentation.js
+++
@@ -1,7 +0,0 @@
-export async function register() {
-  console.log('registered');
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const {startTrace} = await import('@nvisionjs/trace');
-    startTrace();
-  }
-}
