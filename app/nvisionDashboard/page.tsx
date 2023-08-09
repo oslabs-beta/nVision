85a2f6bf1cb81f0f524ece89d05ecf7590627d50
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-08-09 12:46:15 -0400

    Updated Readme and License

--- app/nvisionDashboard/page.tsx
+++ app/nvisionDashboard/page.tsx
@@ -3,6 +3,7 @@ import Wrapper from './Wrapper';
 async function getFiles() {
   const files = await fetch('http://localhost:3000/api/nvisionParser', {
     method: 'GET',
+    cache: 'no-store'
   });
   const parsedFiles = await files.json();
   return parsedFiles;
