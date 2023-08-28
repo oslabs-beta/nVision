12b94c2c3cdd8a3e22dd50c6fbdd8ba8214a5bb5
Sunny Pacheco <pacheco.sunny@gmail.com>
2023-08-28 18:09:24 -0400

    Testing web instrumentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
+import { FrontendTracer } from '../utils/frontendTracer';
 
 export const metadata = {
   title: 'nVision demo app',
   description: 'created by nVision',
+};
+
+if (typeof window !== 'undefined') {
+  FrontendTracer();
 };
 
 export default function RootLayout({
